d8d3113a2e35a0097d5424ded8f71b4f64dd1196
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-06-26 17:17:54 -0500

    vulkan loader cant be found by rust using cmake includes, needs LD_LIBRARY_PATH

--- packages/envs/mecaps/default.nix
+++ packages/envs/mecaps/default.nix
@@ -39,6 +39,7 @@ mkShell
 
   shellHook = ''
     export LD_LIBRARY_PATH=${pkgs.fontconfig.lib}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${pkgs.vulkan-loader}/lib:$LD_LIBRARY_PATH
 
     # only works for qtbase??
     export QT_PLUGIN_PATH=${pkgs.qt5.qtbase}/lib/qt-5.15.9/plugins:$QT_PLUGIN_PATH
