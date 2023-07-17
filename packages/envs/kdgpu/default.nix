17613d0f6931c5a43e7ce6bfb7f3ba540465a35a
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-07-17 11:26:02 -0400

    use clang for kdgpu, add libclang matching stdenv version

--- packages/envs/kdgpu/default.nix
+++ packages/envs/kdgpu/default.nix
@@ -1,37 +1,43 @@
 {
   mkShell,
+  llvmPackagesForShell ? llvmPackages_14,
+  shellStdenv ? llvmPackagesForShell.stdenv,
   pkgs,
   doxybook2 ? pkgs.KDAB.software.doxybook2,
   vulkan-sdk ? pkgs.KDAB.software.vulkan-sdk,
   kdutils ? pkgs.KDAB.software.kdutils,
+  extraPackages ? [llvmPackagesForShell.libclang],
   ...
 }:
-mkShell {
-  packages = with pkgs; [
-    gnumake
-    cmake
-    pkg-config
-    gdb
-    vulkan-sdk
-    vulkan-validation-layers
-    doxybook2
-    doxygen
-    python311Packages.mkdocs
-    python311Packages.mkdocs-material
-    python311Packages.mkdocs-material-extensions
-    wayland
-    wayland-protocols
-    wayland-scanner
-    kdutils
-    glfw
-    glm
-    libxkbcommon
-    xorg.libxcb
-    xorg.libXau
-    xorg.libXdmcp
-    libffi
-  ];
+(mkShell {
+  packages = with pkgs;
+    [
+      gnumake
+      cmake
+      pkg-config
+      gdb
+      vulkan-sdk
+      vulkan-validation-layers
+      doxybook2
+      doxygen
+      python311Packages.mkdocs
+      python311Packages.mkdocs-material
+      python311Packages.mkdocs-material-extensions
+      wayland
+      wayland-protocols
+      wayland-scanner
+      kdutils
+      glfw
+      glm
+      libxkbcommon
+      xorg.libxcb
+      xorg.libXau
+      xorg.libXdmcp
+      libffi
+    ]
+    ++ extraPackages;
   shellHook = ''
     export VULKAN_SDK="${vulkan-sdk}"
   '';
-}
+})
+.override {stdenv = shellStdenv;}
