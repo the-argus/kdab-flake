9b2bac4af800f098f5a405fb0ef11619d613b6c5
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-08-14 23:25:01 -0500

    add environment variables to vulkan memory allocator build

--- packages/software/vulkan-memory-allocator/default.nix
+++ packages/software/vulkan-memory-allocator/default.nix
@@ -1,40 +1,50 @@
 {
   stdenv,
+  lib,
   fetchFromGitHub,
   cmake,
   KDAB,
   vulkan-sdk ? KDAB.software.vulkan-sdk,
   ...
-}:
-stdenv.mkDerivation {
-  pname = "vulkan-memory-allocator";
-  version = "unstable";
-
-  src = fetchFromGitHub {
-    repo = "VulkanMemoryAllocator";
-    owner = "GPUOpen-LibrariesAndSDKs";
-    rev = "29d492b60c84ca784ea0943efc7d2e6e0f3bdaac";
-    hash = "sha256-1YOZ3kZBoxvDjPsJ2PiyR0l5zfkugtcPC6Wg3TuUnCQ=";
-  };
-
-  buildInputs = [
-    cmake
+}: let
+  offDefines = [
+    "VMA_RECORDING_ENABLED"
+    "VMA_USE_STL_CONTAINERS"
+    "VMA_STATIC_VULKAN_FUNCTIONS"
+    "VMA_DYNAMIC_VULKAN_FUNCTIONS"
+    "VMA_DEBUG_ALWAYS_DEDICATED_MEMORY"
+    "VMA_DEBUG_INITIALIZE_ALLOCATIONS"
+    "VMA_DEBUG_GLOBAL_MUTEX"
+    "VMA_DEBUG_DONT_EXCEED_MAX_MEMORY_ALLOCATION_COUNT"
   ];
+in
+  stdenv.mkDerivation
+  {
+    pname = "vulkan-memory-allocator";
+    version = "unstable";
 
-  nativeBuildInputs = [
-    vulkan-sdk
-  ];
+    src = fetchFromGitHub {
+      repo = "VulkanMemoryAllocator";
+      owner = "GPUOpen-LibrariesAndSDKs";
+      rev = "29d492b60c84ca784ea0943efc7d2e6e0f3bdaac";
+      hash = "sha256-1YOZ3kZBoxvDjPsJ2PiyR0l5zfkugtcPC6Wg3TuUnCQ=";
+    };
 
-  VULKAN_SDK = "${vulkan-sdk}";
+    buildInputs = [
+      cmake
+    ];
 
-  cmakeFlags = [
-    "-DVMA_RECORDING_ENABLED=OFF"
-    "-DVMA_USE_STL_CONTAINERS=OFF"
-    "-DVMA_STATIC_VULKAN_FUNCTIONS=OFF"
-    "-DVMA_DYNAMIC_VULKAN_FUNCTIONS=OFF"
-    "-DVMA_DEBUG_ALWAYS_DEDICATED_MEMORY=OFF"
-    "-DVMA_DEBUG_INITIALIZE_ALLOCATIONS=OFF"
-    "-DVMA_DEBUG_GLOBAL_MUTEX=OFF"
-    "-DVMA_DEBUG_DONT_EXCEED_MAX_MEMORY_ALLOCATION_COUNT=OFF"
-  ];
-}
+    nativeBuildInputs = [
+      vulkan-sdk
+    ];
+
+    VULKAN_SDK = "${vulkan-sdk}";
+
+    cmakeFlags =
+      (builtins.map (flag: "-D${flag}=OFF") offDefines)
+      ++ [
+        "-DPOSITION_INDEPENDENT_CODE=ON"
+      ];
+  }
+  # also do environment variable definitions of 0 for all flags
+  // (lib.attrsets.genAttrs offDefines (_: 0))
